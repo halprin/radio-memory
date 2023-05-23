1434c60533cb8e33e059b193ff3527c752dbd626
halprin <halprin@users.noreply.github.com>
2023-05-22 22:34:22 -0600

    Display a button

--- windows/memoryList.go
+++ windows/memoryList.go
@@ -1,19 +1,32 @@
 package windows
 
 import (
+	"log"
+
+	"gioui.org/widget"
+
+	"gioui.org/font/gofont"
+	"gioui.org/layout"
+	"gioui.org/widget/material"
+
 	"gioui.org/app"
 	"gioui.org/io/system"
-	"log"
+	"gioui.org/op"
 )
 
 type MemoryList struct {
-	window *app.Window
+	window     *app.Window
+	operations op.Ops
+	theme      *material.Theme
+	button     *widget.Clickable
 }
 
 func (receiver MemoryList) Display() {
 	log.Println("Starting the memory list window")
 
 	receiver.window = app.NewWindow(app.Title("Memories"))
+	receiver.theme = material.NewTheme(gofont.Collection())
+	receiver.button = &widget.Clickable{}
 
 	go receiver.eventLoop()
 }
@@ -22,7 +35,7 @@ func (receiver MemoryList) eventLoop() {
 	for event := range receiver.window.Events() {
 		switch event := event.(type) {
 		case system.FrameEvent:
-			receiver.draw()
+			receiver.draw(event)
 		case system.DestroyEvent:
 			log.Println("Destroying the memory list window")
 			if event.Err != nil {
@@ -33,6 +46,13 @@ func (receiver MemoryList) eventLoop() {
 	}
 }
 
-func (receiver MemoryList) draw() {
+func (receiver MemoryList) draw(event system.FrameEvent) {
+	context := layout.NewContext(&receiver.operations, event)
 
+	layout.Flex{
+		Axis:    layout.Vertical,
+		Spacing: layout.SpaceEnd,
+	}.Layout(context, layout.Rigid(material.Button(receiver.theme, receiver.button, "Moof!").Layout))
+
+	event.Frame(context.Ops)
 }
